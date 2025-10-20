cdaab61bde2a0aac98bdd10ff24f7f7b001e03d1
MATEUS MACEDO SANTOS <025.029562@universidadesenaicimatec.edu.br>
2025-10-20 11:15:21 -0300

    Deixando o formulário, da página contato, funcional

--- js/form.js
+++ js/form.js
@@ -3,25 +3,36 @@
 
 class contato {
     
+    constructor(nome, sobrenome, email, cpf, telefone, contato){
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.contato = contato;
+    }
 }
 
 function Post(form) {
-
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
+    event.preventDefault();
+    let data = new contato(form.elements.namedItem("nome").value,
+        form.elements.namedItem("sobrenome").value, 
+        form.elements.namedItem("email").value, 
+        form.elements.namedItem("cpf").value, 
+        form.elements.namedItem("telefone").value, 
+        form.elements.namedItem("contato").value);
+    
+    console.log(data);
+    alert(`Obrigado ${data.nome} ${data.sobrenome}, os seus dados foram salvos com sucesso`);
+    form.reset;
 }
 
-function Enviar() {
+// function Enviar() {
 
-    var nome = document.getElementById("nomeid");
+//     var nome = document.getElementById("nomeid");
 
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
-    }
+//     if (nome.value != "") {
+//         alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+//     }
 
-}
+// }
